b1b09e52bfa1732ffbb42626816ca7beaba58a41
Edmund Xin <mxinburritos@gmail.com>
2020-11-26 01:52:23 -0800

    Initial Commit

---
+++ war-card-game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  war-card-game
+//
+//  Created by Edmund Xin on 11/26/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ war-card-game/war_card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_card_gameApp.swift
+//  war-card-game
+//
+//  Created by Edmund Xin on 11/26/20.
+//
+
+import SwiftUI
+
+@main
+struct war_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
