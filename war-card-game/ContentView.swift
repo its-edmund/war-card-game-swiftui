588fe3edef4887d6d99a516a19aa80afbf5077d7
Edmund Xin <mxinburritos@gmail.com>
2020-11-27 02:56:25 -0800

    Cards and logo added

--- war-card-game/ContentView.swift
+++ war-card-game/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background").resizable().ignoresSafeArea(.all)
+            VStack {
+                Image("logo")
+                Spacer()
+                HStack {
+                    Image("back")
+                    Image("back")
+                }
+                Spacer()
+            }
+        }
     }
 }
 
