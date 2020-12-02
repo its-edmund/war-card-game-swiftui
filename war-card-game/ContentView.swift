8092b152aecd76c5e244e7ab231c99334ee9b298
Edmund Xin <mxinburritos@gmail.com>
2020-12-01 16:14:54 -0800

    UI completed

--- war-card-game/ContentView.swift
+++ war-card-game/ContentView.swift
@@ -10,13 +10,46 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Image("background").resizable().ignoresSafeArea(.all)
+            Image("Background").resizable().ignoresSafeArea(.all)
             VStack {
                 Image("logo")
                 Spacer()
                 HStack {
                     Image("back")
                     Image("back")
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                }) {
+                    Image("dealbutton")
+                        .renderingMode(.original)
+                }
+
+                Spacer()
+                
+                HStack {
+                    VStack {
+                        Text("Player")
+                            .bold()
+                            .padding(.bottom, 20)
+                        Text("0")
+                    }
+                    .padding(.leading, 20)
+                    .foregroundColor(.white)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("CPU")
+                            .bold()
+                            .padding(.bottom, 20)
+                        Text("0")
+                    }
+                    .padding(.trailing, 20)
+                    .foregroundColor(.white)
                 }
                 Spacer()
             }
