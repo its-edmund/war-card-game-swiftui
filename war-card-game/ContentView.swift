38f559612aa7dac60217e79e2a306556363f2413
Edmund Xin <mxinburritos@gmail.com>
2020-12-01 16:27:18 -0800

    logic completed

--- war-card-game/ContentView.swift
+++ war-card-game/ContentView.swift
@@ -8,21 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var randNum1 = 2
+    @State private var randNum2 = 2
+    
+    @State private var score1 = 0
+    @State private var score2 = 0
+    
     var body: some View {
+        
         ZStack {
             Image("Background").resizable().ignoresSafeArea(.all)
             VStack {
                 Image("logo")
                 Spacer()
                 HStack {
-                    Image("back")
-                    Image("back")
+                    Image("card" + String(randNum1))
+                    Image("card" + String(randNum2))
                 }
                 
                 Spacer()
                 
                 Button(action: {
+                    self.randNum1 = Int.random(in: 2...14)
+                    self.randNum2 = Int.random(in: 2...14)
                     
+                    if self.randNum1 > self.randNum2 {
+                        self.score1 += 1
+                    }
+                    else if self.randNum2 > self.randNum1 {
+                        self.score2 += 1
+                    }
                 }) {
                     Image("dealbutton")
                         .renderingMode(.original)
@@ -35,7 +51,7 @@ struct ContentView: View {
                         Text("Player")
                             .bold()
                             .padding(.bottom, 20)
-                        Text("0")
+                        Text(String(score1))
                     }
                     .padding(.leading, 20)
                     .foregroundColor(.white)
@@ -46,7 +62,7 @@ struct ContentView: View {
                         Text("CPU")
                             .bold()
                             .padding(.bottom, 20)
-                        Text("0")
+                        Text(String(score2))
                     }
                     .padding(.trailing, 20)
                     .foregroundColor(.white)
